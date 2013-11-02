5f59b25309520732fa341078cfcf9cc896d5dd90
Bruno Campos <brubu88@gmail.com>
2013-11-02 11:41:06 -0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Colchonet::Application.config.secret_key_base = '17dc14e2e9b817e317ea8c92c422be0ff08521ba869e1f5b6f78601fd4ec3b896ad9dc7a39e057d43b5d43bf7034353b0fa60db3a7c1371b693265818f796b7d'
